61638ecc13d474adf085a82197e757fbb16c6795
Mhiretbekalu <106778156+Mhiretbekalu@users.noreply.github.com>
2022-12-20 01:53:03 +0200

    jhdajshdasjhdajshd

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const args = process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
-  const sl = args[(args.length - 2)];
-  console.log(sl);
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const langs = 'C is fun$Python is cool$JavaScript is amazing'.split('$');
-langs.forEach((el) => {
-  console.log(el);
-});
